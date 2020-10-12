1d7997dc56bf292887d58ceed6b3a7d837576117
Pratik-H <hirve.pratik1@gmail.com>
2020-10-12 17:36:13 +0530

    initial commit

---
+++ VaradhiInterviewDemo/VaradhiInterviewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VaradhiInterviewDemo
+//
+//  Created by Neosoft on 12/10/20.
+//  Copyright © 2020 Pratik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
